4f69458e1dd38a1218f431f1123d7f50467f5032
kanersps <12296463+kanersps@users.noreply.github.com>
2021-09-07 08:48:28 +0200

    feature: support appending to an array

--- object/builtins/builtins.go
+++ object/builtins/builtins.go
@@ -15,10 +15,25 @@ var Functions = map[string]*object.Builtin{
 			arg, ok := args[0].(*object.String)
 
 			if !ok {
-				return &object.Error{Message: fmt.Sprintf("ARGUMENT INVALID TYPE TO BUILT-IN FUNCTION `len`. got=%v", args[0].Type())}
+				return &object.Error{Message: fmt.Sprintf("ARGUMENT INVALID TYPE TO BUILT-IN FUNCTION `len`. got=%v. epxected=STRING", args[0].Type())}
 			}
 
 			return &object.Integer{Value: int64(len(arg.Value))}
+		},
+	},
+	"append": {
+		Func: func(args ...object.Object) object.Object {
+			if len(args) <= 0 || len(args) == 1 {
+				return &object.Error{Message: fmt.Sprintf("WRONG NUMBER OF ARGUMENTS TO BUILT-IN FUNCTION `append`. expected=2. got=%d", len(args))}
+			}
+
+			array, ok := args[0].(*object.Array)
+
+			if !ok {
+				return &object.Error{Message: fmt.Sprintf("ARGUMENT INVALID TYPE TO BUILT-IN FUNCTION `append` (argument 0). expected=ARRAY. got=%v", args[0])}
+			}
+
+			return &object.Array{Elements: append(array.Elements, args[1:]...)}
 		},
 	},
 }
